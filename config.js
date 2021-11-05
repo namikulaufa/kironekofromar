fc654a9bfa54da843c957ac05a76e3b9b019d0a6
namikulaufa <85009935+namikulaufa@users.noreply.github.com>
2021-11-05 14:15:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const fs = require("fs")
 userbot = {
 Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "62895-6229-34562" ],
-  MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
+ owner: [ "6285217835752", "380946298137","6289646548836" ],
+  MONGO_URI: "mongodb+srv://yufadanis110903@gmail.com:@Oryzhasativa03@cluster0.eyx0e.mongodb.net/Cluster0?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
    api: "Maaf terjadi kesalahan", 
@@ -33,7 +33,7 @@ Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
    limit: 100,
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
-   butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU',
+   butmag: 'https://i.ibb.co/XJHWhvb/380-94-629-8137-20211105-110105.jpg',
    packname: "I S O X   B O T",
    author: 'Arifi Razzaq',
     setting: {
